fe39c704fc3f4fb24beab12af93e6effaf6a2752
Gabriel <soaresgabrielgsn@gmail.com>
2024-02-28 14:58:16 -0300

    jogodavelha

---
+++ README.md
@@ -0,0 +1,37 @@
+# Jogo da Velha
+
+![Imagem do Jogo da Velha](/img/jogodavelha.png)
+
+Este é um simples projeto de Jogo da Velha desenvolvido em HTML, CSS e JavaScript. O objetivo do jogo é alcançar uma linha, coluna ou diagonal completa com as peças "X" ou "O". Abaixo estão os detalhes sobre o funcionamento e como utilizar o jogo.
+
+## Ferramentas Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Como Funciona
+
+1. Clique no botão "Iniciar Jogo" para habilitar os campos de jogo.
+2. Clique nas células para realizar sua jogada.
+3. O jogo alterna automaticamente entre as peças "X" (jogador) e "O" (máquina).
+4. O resultado do jogo será exibido na parte inferior após cada partida.
+5. Se desejar começar um novo jogo, clique no botão "Novo Jogo".
+
+### Regras do Jogo
+
+- O jogador é representado por "X" e a máquina por "O".
+- O jogo continua até que haja um vencedor ou empate.
+- Um jogador vence se conseguir uma linha, coluna ou diagonal completa.
+- O jogo resulta em empate se todas as células estiverem preenchidas e nenhum jogador venceu.
+
+## Exemplo de Uso
+
+1. Clique no botão "Iniciar Jogo".
+2. Clique nas células para realizar suas jogadas.
+3. O resultado do jogo será exibido automaticamente.
+4. Se desejar iniciar um novo jogo, clique no botão "Novo Jogo".
+
+*Lembre-se de que este Jogo da Velha foi desenvolvido para fins educacionais e pode ser ajustado conforme necessário. Sinta-se à vontade para utilizar e modificar o código de acordo com suas preferências. Se tiver sugestões ou melhorias, ficaremos felizes em recebê-las.*
+
+*Desenvolvido por Gabriel.*
--- script.js
+++ script.js
@@ -1,29 +1,250 @@
-const tabuleiro = [
+const tabela = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 
-function vezDoJogador(linha, coluna) {
-    const celula = tabuleiro[linha][coluna];
-    if (celula && celula.value === "") {
+let celula;
+let jogador;
+let maquina;
+let resultado = document.getElementById('res');
+const corVitoria = "lightblue";
+const corDerrota = "lightcoral";
+const corDaCelula = "white";
+
+function iniciarJogo () {
+    
+    for(let l = 0; l < 3; l++){
+        for(let c = 0; c < 3; c++){
+            const celulas = `celula-${l * 3 + c + 1}`;
+            const classeCelula = document.getElementById(celulas);
+
+            classeCelula.addEventListener('click', () => vezDoJogador(l, c));
+
+            tabela[l][c] = classeCelula;
+            tabela[l][c].style.backgroundColor = corDaCelula;
+            tabela[l][c].removeAttribute("disabled");
+            classeCelula.setAttribute('readonly', true);
+        };
+    };
+
+    let inicio = document.getElementById('iniciar');
+    inicio.setAttribute('disabled', true);
+};
+
+function vezDoJogador(linha, coluna){
+    
+    celula = tabela[linha][coluna];
+    if(celula.value === ""){
         celula.value = "X";
+        celula.setAttribute('disabled', true);
+        celula.style.opacity = "1";
+    
+
+        vezDaMaquina();
+        verificarJogo();
     }
+};
 
-    vezDaMaquina();
-    verificarJogo();
-}
+function vezDaMaquina(){
 
-function iniciarTabuleiro() {
-    for(let i = 0; i < 3; i++){
-        for(let j = 0; j < 3; j++){
-            const classeCelula = `linha${i + 1}-espaço${j + 1}`;
-            const input = document.querySelector(`.${classeCelula}`);
-            input.addEventListener('click', () => vezDoJogador(i, j));
+    let disponivel = [];
 
-            tabuleiro[i][j] = input;
+    for(let l = 0; l < 3; l++){
+        for(let c = 0; c < 3; c++){
+            if(tabela[l][c].value === ""){
+                disponivel.push({linha: l, coluna: c});
+            }
         }
     }
-}
 
-iniciarTabuleiro();
+    if(disponivel.length > 0){
+        let aleatorio = disponivel[Math.floor(Math.random() * disponivel.length)];
+
+        celula = tabela[aleatorio.linha][aleatorio.coluna];
+
+        celula.value = "O";
+        celula.setAttribute('disabled', true);
+        celula.style.opacity = "1";
+    }
+
+};
+
+function verificarJogo(){
+
+    for(let l = 0; l < 3; l++){
+        if(tabela[l][0].value === "X" && tabela[l][1].value === "X" && tabela[l][2].value === "X"){
+            linha();
+            desativarInputs();
+            reiniciarJogo();
+            return;
+        } else if(tabela[l][0].value === "O" && tabela[l][1].value === "O" && tabela[l][2].value === "O"){
+            linha();
+            desativarInputs();
+            reiniciarJogo();
+            return;
+        }
+    }
+
+    for(let c = 0; c < 3; c++){
+        if(tabela[0][c].value === "X" && tabela[1][c].value === "X" && tabela[2][c].value === "X"){
+            coluna();
+            desativarInputs();
+            reiniciarJogo();
+            return;
+        } else if(tabela[0][c].value === "O" && tabela[1][c].value === "O" && tabela[2][c].value === "O"){
+            coluna();
+            desativarInputs();
+            reiniciarJogo();
+            return;
+        }
+    }
+
+    if(tabela[0][0].value === "X" && tabela[1][1].value === "X" && tabela[2][2].value === "X"){
+        diagonal();
+        desativarInputs();
+        reiniciarJogo();
+        return;
+    } else if (tabela[0][2].value === "X" && tabela[1][1].value === "X" && tabela[2][0].value === "X"){
+        diagonal();
+        desativarInputs();
+        reiniciarJogo();
+        return;
+    } else if (tabela[0][0].value === "O" && tabela[1][1].value === "O" && tabela[2][2].value === "O"){
+        diagonal();
+        desativarInputs();
+        reiniciarJogo();
+        return;
+    } else if (tabela[0][2].value === "O" && tabela[1][1].value === "O" && tabela[2][0].value === "O"){
+        diagonal();
+        desativarInputs();
+        reiniciarJogo();
+        return;
+    }
+
+    if(tabela[0][0].value !== "" && tabela[0][1].value !== "" && tabela[0][2].value !== "" && tabela[1][0].value !== "" && tabela[1][1].value !== "" && tabela[1][2].value !== "" && tabela[2][0].value !== "" && tabela[2][1].value !== "" && tabela[2][2].value !== ""){
+        resultado.innerHTML = "Empate";
+        reiniciarJogo();
+    }
+
+};
+
+function linha(){
+    if(tabela[0][0].value === "X" && tabela[0][1].value === "X" && tabela[0][2].value === "X") {
+        tabela[0][0].style.backgroundColor = corVitoria;
+        tabela[0][1].style.backgroundColor = corVitoria;
+        tabela[0][2].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[1][0].value === "X" && tabela[1][1].value === "X" && tabela[1][2].value === "X") {
+        tabela[1][0].style.backgroundColor = corVitoria;
+        tabela[1][1].style.backgroundColor = corVitoria;
+        tabela[1][2].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[2][0].value === "X" && tabela[2][1].value === "X" && tabela[2][2].value === "X") {
+        tabela[2][0].style.backgroundColor = corVitoria;
+        tabela[2][1].style.backgroundColor = corVitoria;
+        tabela[2][2].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[0][0].value === "O" && tabela[0][1].value === "O" && tabela[0][2].value === "O") {
+        tabela[0][0].style.backgroundColor = corDerrota;
+        tabela[0][1].style.backgroundColor = corDerrota;
+        tabela[0][2].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    } else if(tabela[1][0].value === "O" && tabela[1][1].value === "O" && tabela[1][2].value === "O") {
+        tabela[1][0].style.backgroundColor = corDerrota;
+        tabela[1][1].style.backgroundColor = corDerrota;
+        tabela[1][2].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    } else if(tabela[2][0].value === "O" && tabela[2][1].value === "O" && tabela[2][2].value === "O") {
+        tabela[2][0].style.backgroundColor = corDerrota;
+        tabela[2][1].style.backgroundColor = corDerrota;
+        tabela[2][2].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    }
+};
+
+function coluna(){
+    if(tabela[0][0].value === "X" && tabela[1][0].value === "X" && tabela[2][0].value === "X") {
+        tabela[0][0].style.backgroundColor = corVitoria;
+        tabela[1][0].style.backgroundColor = corVitoria;
+        tabela[2][0].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[0][1].value === "X" && tabela[1][1].value === "X" && tabela[2][1].value === "X") {
+        tabela[0][1].style.backgroundColor = corVitoria;
+        tabela[1][1].style.backgroundColor = corVitoria;
+        tabela[2][1].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[0][2].value === "X" && tabela[1][2].value === "X" && tabela[2][2].value === "X") {
+        tabela[0][2].style.backgroundColor = corVitoria;
+        tabela[1][2].style.backgroundColor = corVitoria;
+        tabela[2][2].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[0][0].value === "O" && tabela[1][0].value === "O" && tabela[2][0].value === "O") {
+        tabela[0][0].style.backgroundColor = corDerrota;
+        tabela[1][0].style.backgroundColor = corDerrota;
+        tabela[2][0].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    } else if(tabela[0][1].value === "O" && tabela[1][1].value === "O" && tabela[2][1].value === "O") {
+        tabela[0][1].style.backgroundColor = corDerrota;
+        tabela[1][1].style.backgroundColor = corDerrota;
+        tabela[2][1].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    } else if(tabela[0][2].value === "O" && tabela[1][2].value === "O" && tabela[2][2].value === "O") {
+        tabela[0][2].style.backgroundColor = corDerrota;
+        tabela[1][2].style.backgroundColor = corDerrota;
+        tabela[2][2].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    }
+};
+
+function diagonal(){
+    if(tabela[0][0].value === "X" && tabela[1][1].value === "X" && tabela[2][2].value === "X"){
+        tabela[0][0].style.backgroundColor = corVitoria;
+        tabela[1][1].style.backgroundColor = corVitoria;
+        tabela[2][2].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[0][2].value === "X" && tabela[1][1].value === "X" && tabela[2][0].value === "X"){
+        tabela[0][2].style.backgroundColor = corVitoria;
+        tabela[1][1].style.backgroundColor = corVitoria;
+        tabela[2][0].style.backgroundColor = corVitoria;
+        resultado.innerHTML = "Você ganhou";
+    } else if(tabela[0][0].value === "O" && tabela[1][1].value === "O" && tabela[2][2].value === "O"){
+        tabela[0][0].style.backgroundColor = corDerrota;
+        tabela[1][1].style.backgroundColor = corDerrota;
+        tabela[2][2].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    } else if(tabela[0][2].value === "O" && tabela[1][1].value === "O" && tabela[2][0].value === "O"){
+        tabela[0][2].style.backgroundColor = corDerrota;
+        tabela[1][1].style.backgroundColor = corDerrota;
+        tabela[2][0].style.backgroundColor = corDerrota;
+        resultado.innerHTML = "Você perdeu";
+    }
+};
+
+function reiniciarJogo(){
+    let reiniciar = document.getElementById("reiniciar");
+    reiniciar.removeAttribute("disabled");
+};
+
+function novoJogo(){
+    for(let l = 0; l < 3; l++){
+        for(let c = 0; c < 3; c++){
+            tabela[l][c].value = "";
+            tabela[l][c].removeAttribute("disabled");
+            tabela[l][c].style.backgroundColor = corDaCelula;
+        }
+    }
+
+    resultado.innerHTML = "Resultado";
+
+    let reiniciar = document.getElementById("reiniciar");
+    reiniciar.setAttribute("disabled", true)
+};
+
+function desativarInputs(){
+    for(let l = 0; l < 3; l++){
+        for(let c = 0; c < 3; c++){
+            tabela[l][c].setAttribute('disabled', true);
+        }
+    }
+};
